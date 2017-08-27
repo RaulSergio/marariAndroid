ce047aefad4bfe33a1a52f108dad11a057c5e06f
RaulSergio <dragolaymann@gmail.com>
2017-08-27 19:26:38 -0400

    cadastro cliente direto no banco e leitor de codigo de barras implementado

--- app/src/main/java/ucdb/br/sweccamobile/TelaCadastroCliente.java
+++ app/src/main/java/ucdb/br/sweccamobile/TelaCadastroCliente.java
@@ -1,11 +1,26 @@
 package ucdb.br.sweccamobile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TelaCadastroCliente extends AppCompatActivity {
+
+    EditText txtNome, txtTelefone, txtCpfCnpj, txtEmail, txtEndereco, txtNumero, txtCidade, txtObs;
+    Button btnCadastrar;
+
+    String url = "";
+    String parametros = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +29,78 @@ public class TelaCadastroCliente extends AppCompatActivity {
         getSupportActionBar().setTitle("Cadastro de clientes");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        txtNome = (EditText) findViewById(R.id.edtTxtNomeCadastroCliente);
+        txtTelefone = (EditText) findViewById(R.id.edtTxtTelefoneCadastroCliente);
+        txtCpfCnpj = (EditText) findViewById(R.id.edtTxtCpfCnpjCadastroCliente);
+        txtEmail = (EditText) findViewById(R.id.edtTxtEmailCadastroCliente);
+        txtEndereco = (EditText) findViewById(R.id.edtTxtEnderecoCadastroCliente);
+        txtNumero = (EditText) findViewById(R.id.edtTxtNumeroCadastroCliente);
+        txtCidade = (EditText) findViewById(R.id.edtTxtCidadeCadastroCliente);
+        txtObs = (EditText) findViewById(R.id.edtTxtObsCadastro);
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrarCliente);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectivityManager connectivityManager = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if (networkInfo != null && networkInfo.isConnected()) {
+
+                    String nome = txtNome.getText().toString();
+                    String telefone = txtTelefone.getText().toString();
+                    String cpfCnpj = txtCpfCnpj.getText().toString();
+                    String email = txtEmail.getText().toString();
+                    String endereco = txtEmail.getText().toString();
+                    String numero = txtNumero.getText().toString();
+                    String cidade = txtCidade.getText().toString();
+                    String obs = txtObs.getText().toString();
+
+
+                    if (nome.isEmpty() || telefone.isEmpty() || cpfCnpj.isEmpty() || email.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "Alguns campos são obrigatórios!", Toast.LENGTH_LONG).show();
+                    } else {
+                        //criar a classe de cadastro de clientes
+                        url = "http://192.168.1.6:80/login/cadastroCliente.php";
+
+                        parametros = "nome=" + nome + "&telefone=" + telefone + "&cpfCnpj=" + cpfCnpj + "&email=" + email +
+                                "&endereco=" + endereco + "&numero=" + numero + "&cidade=" + cidade + "&obs=" + obs;
+
+                        new TelaCadastroCliente.SolicitaDados().execute(url);
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Nenhuma conexão foi detectada!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
+
+    private class SolicitaDados extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return Conexao.postDados(urls[0], parametros);
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            //criar condicao na classe de cadastro de clientes
+            if (resultado.contains("cpf/cnpj_erro")) {
+                Toast.makeText(getApplicationContext(), "Já existe um cliente cadastrado para este CPF/CNPJ!", Toast.LENGTH_LONG).show();
+
+            } else if (resultado.contains("registro_ok")) {
+                Toast.makeText(getApplicationContext(), "Cliente cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+                Intent abreClientes = new Intent(TelaCadastroCliente.this, TelaClientes.class);
+                startActivity(abreClientes);
+            } else {
+                Toast.makeText(getApplicationContext(), "Ocorreu um erro!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -24,6 +110,7 @@ public class TelaCadastroCliente extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
--- app/src/main/java/ucdb/br/sweccamobile/TelaProdutos.java
+++ app/src/main/java/ucdb/br/sweccamobile/TelaProdutos.java
@@ -1,5 +1,6 @@
 package ucdb.br.sweccamobile;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,31 +8,74 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class TelaProdutos extends AppCompatActivity {
+    public static final int REQUEST_CODE = 0;
+    private TextView txtResult;
 
-    Button botaoBuscaproduto;
+    Button botaoBuscaproduto, botaoEscaneiaCodigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Activity activity = this;
         setContentView(R.layout.activity_tela_produtos);
         getSupportActionBar().setTitle("Produtos");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        txtResult = (TextView) findViewById(R.id.txtNomeProduto);
+
         botaoBuscaproduto = (Button) findViewById(R.id.btnBuscaProduto);
+        botaoEscaneiaCodigo = (Button) findViewById(R.id.btnScanea);
 
         botaoBuscaproduto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent abreClientes = new Intent(TelaProdutos.this, CarteiraDeProdutos.class);
-                startActivity(abreClientes);
+                Intent abreProdutos = new Intent(TelaProdutos.this, CarteiraDeProdutos.class);
+                startActivity(abreProdutos);
             }
 
             public void OnClick(View v) {
 
             }
         });
+
+        botaoEscaneiaCodigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Escaneando");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+
+            public void OnClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelando Escaneamento", Toast.LENGTH_LONG).show();
+            } else {
+                //para testar!!
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
@@ -43,6 +87,7 @@ public class TelaProdutos extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
